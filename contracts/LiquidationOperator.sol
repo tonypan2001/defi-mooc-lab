915afb2568d603c1debcdf304bb099745d762df0
Prompan Uechanwech <prompan.u@ku.th>
2023-04-03 18:00:24 +0700

    question 3
    for question 3

--- contracts/LiquidationOperator.sol
+++ contracts/LiquidationOperator.sol
@@ -140,14 +140,19 @@ contract LiquidationOperator is IUniswapV2Callee {
     IERC20 constant WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
     IWETH constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 constant USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     IUniswapV2Factory constant uniswapV2Factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     IUniswapV2Pair immutable uniswapV2Pair_WETH_USDT; // Pool1
     IUniswapV2Pair immutable uniswapV2Pair_WBTC_WETH; // Pool2
+    IUniswapV2Pair immutable uniswapV2Pair_WBTC_USDT; // Pool3
+    IUniswapV2Pair immutable uniswapV2Pair_WETH_USDC; // Pool4
+    IUniswapV2Pair immutable uniswapV2Pair_USDC_WETH; // Pool5
 
     ILendingPool constant lendingPool = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
 
-    address constant liquidationTarget = 0x59CE4a2AC5bC3f5F225439B2993b86B42f6d3e9F;
+    // address constant liquidationTarget = 0x59CE4a2AC5bC3f5F225439B2993b86B42f6d3e9F;
+    address constant liquidationTarget = 0x63f6037d3e9d51ad865056BF7792029803b6eEfD;
     uint debt_USDT;
 
     // END TODO
@@ -194,13 +199,15 @@ contract LiquidationOperator is IUniswapV2Callee {
         // TODO: (optional) initialize your contract
 
         uniswapV2Pair_WETH_USDT = IUniswapV2Pair(uniswapV2Factory.getPair(address(WETH), address(USDT))); // Pool1
-        // uniswapV2Pair_WBTC_USDT = IUniswapV2Pair(uniswapV2Factory.getPair(address(WBTC), address(USDT))); // Pool1
         uniswapV2Pair_WBTC_WETH = IUniswapV2Pair(uniswapV2Factory.getPair(address(WBTC), address(WETH))); // Pool2
-        // uniswapV2Pair_WBTC_USDT = IUniswapV2Pair(uniswapV2Factory.getPair(address(WBTC), address(USDT))); // Pool3
+        uniswapV2Pair_WBTC_USDT = IUniswapV2Pair(uniswapV2Factory.getPair(address(WBTC), address(USDT))); // Pool3
+        uniswapV2Pair_WETH_USDC = IUniswapV2Pair(uniswapV2Factory.getPair(address(WETH), address(USDC))); // Pool4
+        uniswapV2Pair_USDC_WETH = IUniswapV2Pair(uniswapV2Factory.getPair(address(USDC), address(WETH))); // Pool5
         // debt_USDT = 2916378221684;
-        debt_USDT = 10000000000;
+        // debt_USDT = 10000000000;
         // debt_USDT = 5000000000;
         // debt_USDT = 2000000000;
+        debt_USDT = 8128956343; //8,128.956343
         
         // END TODO
     }
@@ -243,8 +250,9 @@ contract LiquidationOperator is IUniswapV2Callee {
         // we should borrow USDT, liquidate the target user and get the WBTC, then swap WBTC to repay uniswap
         // (please feel free to develop other workflows as long as they liquidate the target user successfully)
 
-        uniswapV2Pair_WETH_USDT.swap(0, debt_USDT, address(this), "$");
+        // uniswapV2Pair_WETH_USDT.swap(0, debt_USDT, address(this), "$");
         // uniswapV2Pair_WBTC_USDT.swap(0, debt_USDT, address(this), "$");
+        uniswapV2Pair_WETH_USDC.swap(debt_USDT, 0, address(this), "$");
 
         // 3. Convert the profit into ETH and send back to sender
 
@@ -258,7 +266,7 @@ contract LiquidationOperator is IUniswapV2Callee {
     // required by the swap
     function uniswapV2Call(
         address,
-        uint256,
+        uint256 amount0,
         uint256 amount1,
         bytes calldata
     ) external override {
@@ -266,34 +274,40 @@ contract LiquidationOperator is IUniswapV2Callee {
 
         // 2.0. security checks and initializing variables
         
-        assert(msg.sender == address(uniswapV2Pair_WETH_USDT));
         // assert(msg.sender == address(uniswapV2Pair_WBTC_USDT));
+        assert(msg.sender == address(uniswapV2Pair_WETH_USDC));
         (uint256 reserve_WETH_Pool1, uint256 reserve_USDT_Pool1, ) = uniswapV2Pair_WETH_USDT.getReserves(); // Pool1
-        // (uint256 reserve_WBTC_Pool1, uint256 reserve_USDT_Pool1, ) = uniswapV2Pair_WBTC_USDT.getReserves(); // Pool1
         (uint256 reserve_WBTC_Pool2, uint256 reserve_WETH_Pool2, ) = uniswapV2Pair_WBTC_WETH.getReserves(); // Pool2
-        // (uint256 reserve_WBTC_Pool3, uint256 reserve_USDT_Pool3, ) = uniswapV2Pair_WBTC_USDT.getReserves(); // Pool3
+        (uint256 reserve_WBTC_Pool3, uint256 reserve_USDT_Pool3, ) = uniswapV2Pair_WBTC_USDT.getReserves(); // Pool3
+        (uint256 reserve_USDC_Pool4, uint256 reserve_WETH_Pool4, ) = uniswapV2Pair_WETH_USDC.getReserves(); // Pool4
 
-        // console.log("uniswapV2Pair(%s): WBTC <> USDT", address(uniswapV2Pair_WETH_USDT));
-        // console.log("reserve WBTC: %s", reserve_WBTC_Pool1);
-        // console.log("reserve USDT: %s", reserve_USDT_Pool1);
+        console.log("uniswapV2Pair(%s) : WBTC <> USDT", address(uniswapV2Pair_WBTC_USDT));
+        console.log("Reserve WBTC : %s", reserve_WBTC_Pool3);
+        console.log("Reserve USDT : %s", reserve_USDT_Pool3);
 
-        // console.log("uniswapV2Pair(%s): WBTC <> WETH", address(uniswapV2Pair_WBTC_WETH));
-        // console.log("reserve WBTC: %s", reserve_WBTC_Pool2);
-        // console.log("reserve WETH: %s", reserve_WETH_Pool2);
+        console.log("uniswapV2Pair(%s) : WBTC <> WETH", address(uniswapV2Pair_WBTC_WETH));
+        console.log("Reserve WBTC : %s", reserve_WBTC_Pool2);
+        console.log("Reserve WETH : %s", reserve_WETH_Pool2);
 
         // 2.1 liquidate the target user
         
-        uint debtToCover = amount1;
-        USDT.approve(address(lendingPool), debtToCover);
-        lendingPool.liquidationCall(address(WBTC), address(USDT), liquidationTarget, debtToCover, false);
-        uint collateral_WBTC = WBTC.balanceOf(address(this));
-        // console.log("WBTC balance of address(this): %s", collateral_WBTC);
+        // uint debtToCover = amount1;
+        // USDT.approve(address(lendingPool), debtToCover);
+        // lendingPool.liquidationCall(address(WBTC), address(USDT), liquidationTarget, debtToCover, false);
+        // uint collateral_WBTC = WBTC.balanceOf(address(this));
+
+        uint debtToCover = amount0;
+        USDC.approve(address(lendingPool), debtToCover);
+        lendingPool.liquidationCall(address(WETH), address(USDC), liquidationTarget, debtToCover, false);
+     
+        uint collateral_WETH = WETH.balanceOf(address(this));
+        console.log("WETH balance of address(this): %s", WETH.balanceOf(address(this)));
 
         // 2.2 swap WBTC for other things or repay directly
         
-        WBTC.transfer(address(uniswapV2Pair_WBTC_WETH), collateral_WBTC);
-        uint amountOut_WETH = getAmountOut(collateral_WBTC, reserve_WBTC_Pool2, reserve_WETH_Pool2);
-        uniswapV2Pair_WBTC_WETH.swap(0, amountOut_WETH, address(this), "");
+        // WBTC.transfer(address(uniswapV2Pair_WBTC_WETH), collateral_WBTC);
+        // uint amountOut_WETH = getAmountOut(collateral_WBTC, reserve_WBTC_Pool2, reserve_WETH_Pool2);
+        // uniswapV2Pair_WBTC_WETH.swap(0, amountOut_WETH, address(this), "");
 
         // WBTC.transfer(address(uniswapV2Pair_WBTC_USDT), collateral_WBTC);
         // uint amountOut_USDT = getAmountOut(collateral_WBTC, reserve_WBTC_Pool3, reserve_USDT_Pool3);
@@ -301,8 +315,9 @@ contract LiquidationOperator is IUniswapV2Callee {
 
         // 2.3 repay
 
-        uint repay_WETH = getAmountIn(debtToCover, reserve_WETH_Pool1, reserve_USDT_Pool1);
-        WETH.transfer(address(uniswapV2Pair_WETH_USDT), repay_WETH);
+        uint repay_WETH = getAmountIn(debtToCover, reserve_WETH_Pool4, reserve_USDC_Pool4);
+        WETH.transfer(address(uniswapV2Pair_WETH_USDC), repay_WETH);
+        console.log("Balance (WETH) : %s", WETH.balanceOf(address(this)));
 
         // uint repay_WBTC = getAmountIn(debtToCover, reserve_WBTC_Pool1, reserve_USDT_Pool1);
         // WBTC.transfer(address(uniswapV2Pair_WBTC_USDT), repay_WBTC);
--- test/liquidation.js
+++ test/liquidation.js
@@ -10,7 +10,8 @@ describe("Liquidation", function () {
         params: [{
           forking: {
             jsonRpcUrl: process.env.ALCHE_API,
-            blockNumber: 12489619,
+            // blockNumber: 12489619,
+            blockNumber: 11946807, 
           }
         }]
       });
@@ -36,7 +37,8 @@ describe("Liquidation", function () {
         v => v && v.topics && v.address === '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' && Array.isArray(v.topics) && 
         v.topics.length > 3 && v.topics[0] === '0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286')
 
-    const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f');
+    // const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f');
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x00000000000000000000000063f6037d3e9d51ad865056bf7792029803b6eefd'); 
 
     expect(expectedLiquidationEvents.length, "no expected liquidation").to.be.above(0);
     expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(expectedLiquidationEvents.length);
@@ -47,7 +49,7 @@ describe("Liquidation", function () {
     }));
 
     const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
-    console.log("Profit", utils.formatEther(profit), "ETH");
+    console.log("Profit : ", utils.formatEther(profit), "ETH");
 
     expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
     writeFile('profit.txt', String(utils.formatEther(profit)), function (err) {console.log("failed to write profit.txt: %s", err)});
